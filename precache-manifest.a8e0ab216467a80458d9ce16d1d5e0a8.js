2565905732ddc0ca7ff82a82492ae17efbe2e047
EirianAlin <111126968+EirianAlin@users.noreply.github.com>
2022-10-12 18:36:16 +0300

    Add files via upload

---
+++ precache-manifest.a8e0ab216467a80458d9ce16d1d5e0a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd455aae91c911a0a382c2032d992725",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9128b16c6d52ec6f08d",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "99b30bb7c51c92114d1b",
+    "url": "/static/js/2.4589ff64.chunk.js"
+  },
+  {
+    "revision": "a9128b16c6d52ec6f08d",
+    "url": "/static/js/main.4f0bc180.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
